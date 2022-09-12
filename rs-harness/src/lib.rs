d56d06f396ced70d52cf96374f74b28f1d3c1168
cbrzn <cesarbrazon10@gmail.com>
2022-09-12 22:32:44 +0200

    feat: implementation files with folder, schema & dependency file done

---
+++ rs-harness/src/lib.rs
@@ -0,0 +1,26 @@
+use lazy_static::lazy_static;
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub struct Implementation<'a> {
+    pub dependency: &'a str,
+    pub name: &'a str,
+    pub module: &'a str
+}
+
+lazy_static! {
+    pub static ref IMPLEMENTATIONS: HashMap<&'static str, Implementation<'static>> = {
+        let mut map = HashMap::new();
+        map.insert("as", Implementation {
+            dependency: "package.json",
+            name: "assemblyscript",
+            module: "src/index.ts"
+        });
+        map.insert("rs", Implementation {
+            dependency: "Cargo.toml",
+            name: "rust",
+            module: "Cargo.toml"
+        });
+        return map
+    };
+}
--- rs-harness/src/main.rs
+++ rs-harness/src/main.rs
@@ -1,7 +1,11 @@
-use std::env::current_exe;
+mod lib;
+
+use crate::lib::IMPLEMENTATIONS;
+
 use std::{fs, io};
-use std::ffi::OsString;
-use std::fmt::format;
+use std::fs::File;
+use std::io::BufReader;
+use std::ops::Index;
 use std::path::Path;
 use serde_json;
 use serde_yaml;
@@ -11,7 +15,10 @@ use serde::{Deserialize, Serialize};
 const BUILD_FOLDER: &str = "build";
 const TEST_FOLDER: &str = "tests";
 
+const CUSTOM_MANIFEST: &str = "polywrap.json";
+
 const EXPECTED_FILES: [&str; 3] = ["workflow.json", "schema.graphql", "implementations"];
+
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 struct Workflow {
@@ -19,6 +26,45 @@ struct Workflow {
     name: Option<String>,
     jobs: serde_json::Value,
 }
+
+type ImportAbis = Vec<ImportAbi>;
+#[derive(Debug, Default, Serialize, Deserialize)]
+struct ImportAbi {
+    uri: String,
+    abi: String
+}
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+struct Project {
+    name: Option<String>,
+    _type: Option<String>
+}
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+struct Source {
+    schema: Option<String>,
+    module: Option<String>,
+    import_abis: Option<ImportAbis>
+}
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+struct Manifest {
+    format: Option<String>,
+    project: Option<Project>,
+    source: Option<Source>
+}
+
+// impl Manifest {
+//     fn merge(self, other: Manifest) -> Self {
+//         Self {
+//             format: self.format,
+//             project: Project {
+//                 ..self.project
+//             },
+//             source: self.source | other.source
+//         }
+//     }
+// }
 
 impl Generator<'_> {
     pub fn new<'a>(
@@ -51,14 +97,18 @@ impl Generator<'_> {
             panic!("{}", error_message);
         }
 
-        fs::create_dir(project_folder).expect("TODO: panic message");
+        fs::create_dir(project_folder).unwrap();
 
-        self.generate_test_manifest(project_name).expect("TODO: panic message");
+        // Generate test manifest from workflow
+        self.generate_test_manifest(project_name).unwrap();
+        // Copy schema to implementation folder
+        self.generate_schema(project_name).unwrap();
+        self.generate_dependency_file(project_name).unwrap();
 
         Ok(())
     }
 
-    pub fn generate_test_manifest(&self, project_name: &str) -> Result<(), &str> {
+    pub fn generate_test_manifest(&self, project_name: &str) -> Result<(), ()> {
         let workflow_path = self.source_path.join(project_name).join(EXPECTED_FILES[0]);
         let workflow_str = fs::read_to_string(&workflow_path).unwrap();
         let mut workflow: Workflow = serde_json::from_str(&workflow_str.as_str()).unwrap();
@@ -66,12 +116,7 @@ impl Generator<'_> {
         workflow.format = Some(String::from("0.1.0"));
         workflow.name = Some(String::from(project_name));
 
-        dbg!(&workflow);
-        let manifest_json = serde_json::to_string(&workflow).unwrap();
-        dbg!(&manifest_json);
-        let manifest: Workflow = serde_yaml::from_str(&manifest_json).unwrap();
-        dbg!(&manifest);
-
+        // TODO: Add validation to test manifest from JS
         let test_manifest_path = self.dest_path.join(project_name).join("polywrap.test.yaml");
         let f = fs::OpenOptions::new()
             .write(true)
@@ -79,13 +124,91 @@ impl Generator<'_> {
             .open(&test_manifest_path)
             .unwrap();
 
-        serde_yaml::to_writer(f, &workflow);
+        serde_yaml::to_writer(f, &workflow).expect("TODO: panic message");
+        Ok(())
+    }
 
+    pub fn generate_schema(&self, project_name: &str) -> Result<(), ()> {
+        let source_path = self.source_path.join(project_name).join("schema.graphql");
+        let dest_path = self.dest_path.join(project_name).join("schema.graphql");
+        fs::copy(source_path, dest_path).unwrap();
+        Ok(())
+    }
 
-        dbg!(test_manifest_path);
+    pub fn generate_dependency_file(&self, project_name: &str) -> Result<(), ()> {
+        let dest_implementation_folder = &self.dest_path.join(project_name).join("implementations");
+        fs::create_dir(dest_implementation_folder).unwrap();
+        let template_implementation_folder = &self.source_path.join(project_name).join("implementations");
 
-        // fs::write(test_manifest_path, manifest).expect("TODO: panic message");
+        let implementations = fs::read_dir(template_implementation_folder).unwrap();
+        for implementation in implementations {
+            let imp = implementation.unwrap();
+            let impl_name = &imp.file_name();
+            let source_path = &template_implementation_folder.join(impl_name);
+            let dest_path = &dest_implementation_folder.join(impl_name).join("src");
 
+            // Generate implementation files (i.e: index.ts/lib.rs)
+            let files = fs::read_dir(source_path).unwrap();
+            for file in files {
+                fs::create_dir_all(dest_path).unwrap();
+                let name = &file.as_ref().unwrap().file_name();
+                let impl_source = source_path.join(name);
+                let impl_dest = dest_path.join(name);
+                fs::copy(impl_source, impl_dest).unwrap();
+            }
+
+            // Generate dependency files (i.e: package.json/Cargo.toml)
+            let defaults_folder = self.source_path.join("..").join("defaults");
+            let implementation_info = IMPLEMENTATIONS.get(impl_name.to_str().unwrap()).unwrap();
+            let dependencies_source = defaults_folder.join(&implementation_info.dependency);
+            let dependencies_dest = dest_implementation_folder.join(impl_name).join(&implementation_info.dependency);
+            fs::copy(dependencies_source, dependencies_dest).unwrap();
+
+            let root = template_implementation_folder.join("..");
+            let root_files = fs::read_dir(root).unwrap().into_iter().filter(
+                |file| {
+                    return !EXPECTED_FILES.to_vec().contains(&file.as_ref().unwrap().file_name().to_str().unwrap())
+                }
+            ).map(|entry| entry.unwrap()).collect::<Vec<_>>();
+
+            // Generate polywrap manifest (i.e: polywrap.yaml)
+            let project = Project {
+                name: Some(project_name.to_string()),
+                _type: Some(String::new()),
+            };
+
+            let source = Source {
+                schema: Some(String::new()),
+                module: Some(String::new()),
+                import_abis: None
+            };
+
+            let manifest = Manifest {
+                format: Some(String::new()),
+                project: Some(project),
+                source: Some(source)
+            };
+
+            let index = root_files.iter().position(|file| {
+                return file.file_name().eq(CUSTOM_MANIFEST);
+            });
+
+            match index {
+                Some(i) => {
+                    // Open the file in read-only mode with buffer.
+                    let file = File::open(root_files[i].path()).unwrap();
+                    let reader = BufReader::new(file);
+
+                    let custom_manifest: Result<serde_json::Value, serde_json::Error> = serde_json::from_reader(reader);
+                    let manifest = serde_json::to_string(&manifest);
+
+                    println!("{:?}", custom_manifest.unwrap());
+                    println!("{:?}", &manifest.unwrap())
+
+                },
+                _ => {}
+            }
+        }
 
         Ok(())
     }
@@ -96,8 +219,8 @@ struct Generator<'a> {
     pub source_path: &'a Path,
 }
 
-fn main() -> std::io::Result<()> {
-    let dirname = current_exe()?;
+fn main() -> io::Result<()> {
+    // let dirname = current_exe()?;
 
     let dest_path = Path::new(BUILD_FOLDER);
     let source_path = Path::new(TEST_FOLDER);
@@ -109,7 +232,6 @@ fn main() -> std::io::Result<()> {
         },
         _  => {}
     }
-
 
     let generator = Generator::new(dest_path, source_path);
 
