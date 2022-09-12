1c04d05943ff52db3461544db35fe34b6f801edd
cbrzn <cesarbrazon10@gmail.com>
2022-09-12 17:58:09 +0200

    chore: test manifests generated

--- rs-harness/src/main.rs
+++ rs-harness/src/main.rs
@@ -1,15 +1,24 @@
-use std::borrow::Borrow;
 use std::env::current_exe;
-use std::error::Error;
 use std::{fs, io};
 use std::ffi::OsString;
-use std::fs::{read, read_dir};
+use std::fmt::format;
 use std::path::Path;
+use serde_json;
+use serde_yaml;
+use serde::{Deserialize, Serialize};
+
 
 const BUILD_FOLDER: &str = "build";
 const TEST_FOLDER: &str = "tests";
 
 const EXPECTED_FILES: [&str; 3] = ["workflow.json", "schema.graphql", "implementations"];
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+struct Workflow {
+    format: Option<String>,
+    name: Option<String>,
+    jobs: serde_json::Value,
+}
 
 impl Generator<'_> {
     pub fn new<'a>(
@@ -23,105 +32,62 @@ impl Generator<'_> {
     }
 
     pub fn generate_project(&self, project_name: &str) -> Result<(), &str> {
-        let build_folder = self.dest_path.join(project_name);
+        let project_folder = self.dest_path.join(project_name);
         let test_folder = self.source_path.join(project_name);
 
-        // for entry in read_dir(&test_folder).unwrap() {
-        //     let name = entry.unwrap().file_name();
-        //     let g = name.to_str().unwrap();
-        //     dbg!(g);
-        // }
-
-        // let _ = read_dir(&test_folder).unwrap().contains(&EXPECTED_FILES.to_vec());
-
-        let files = read_dir(&test_folder).unwrap().map(|directory| {
+        let files = fs::read_dir(&test_folder).unwrap().map(|directory| {
             let name = directory.unwrap().file_name();
             return name.into_string().unwrap()
         }).collect::<Vec<_>>();
-
-        // let missing_files = EXPECTED_FILES
-        //     .iter()
-        //     .filter_map(|&file| files.contains(file.into_string()))
-        //     .collect::<Vec<_>>();
-
-
-        // let missing_files = EXPECTED_FILES
-        //     .iter()
-        //     .map(|&file| {
-        //         if files.contains(&String::from(file)) {
-        //             return file.into_string()
-        //         }
-        //         return
-        //     })
-        //     .collect::<Vec<_>>();
 
         let missing_files = EXPECTED_FILES
             .into_iter()
             .filter(|&file| !files.contains(&String::from(file)))
             .collect::<Vec<_>>();
 
-
-        match missing_files.len() {
-            (0) => {
-
-            },
-            (_) => {
-
-            },
+        if missing_files.len() > 0 {
+            let error_message = format!("File {} missing from tests: {}", missing_files[0], project_name);
+            // TODO: Return Err instead of panicking
+            panic!("{}", error_message);
         }
 
-        dbg!("{}", &missing_files);
-        // match missing_files {
-        //     vec!(false, true, true) => {
-        //
-        //     },
-        //     vec!(true, false, true) => {
-        //
-        //     },
-        //     vec!(true, true, false) => {
-        //
-        //     },
-        // }
+        fs::create_dir(project_folder).expect("TODO: panic message");
 
-        println!("test case: {}", project_name);
-        dbg!(missing_files);
-        // let files2 = files.map()
-        // let files = read_dir(&test_folder)?
-        //     .map(|res| res.map(|e| {
-        //         match e.file_name().to_str(). {
-        //             Some(t) => t,
-        //             _ => ""
-        //         }
-        //
-        //     }))
-        //     .collect::<Result<Vec<_>, io::Error>>();
-        //
-        // // dbg!(entries.expect("TODO: panic message"));
-        //
-        dbg!(files);
-        // let has_missing_file = EXPECTED_FILES.to_vec().contains(files.to_vec());
+        self.generate_test_manifest(project_name).expect("TODO: panic message");
 
-        // dbg!(has_missing_file);
-        // let files = &entries.iter().filter_map(|entry| {
-        //     dbg!(entry);
-        //     let entry_str = entry.iter().map(|file| file.to_str()).collect::<Vec<_>>();
-        //     EXPECTED_FILES.to_vec().
-        //     Some(entry_str)
-        // }).collect::<Vec<_>>();
-        // dbg!(zz);
-        // entries.map(|entry| entry.contains(EXPECTED_FILES))?;
-        // let missing_file = EXPECTED_FILES.contains(&&"workflow.json");
-        // println!("{}", missing_file);
+        Ok(())
+    }
 
-        // match missing_file {
-        //     Some(file) => {
-        //         return Err("File {file} missing from tests: {project_name}");
-        //     },
-        //     _ => {}
-        // }
-        // entries.iter().filter_map(|entry| expected_files.contains(entry))
+    pub fn generate_test_manifest(&self, project_name: &str) -> Result<(), &str> {
+        let workflow_path = self.source_path.join(project_name).join(EXPECTED_FILES[0]);
+        let workflow_str = fs::read_to_string(&workflow_path).unwrap();
+        let mut workflow: Workflow = serde_json::from_str(&workflow_str.as_str()).unwrap();
 
-        return Ok(())
+        workflow.format = Some(String::from("0.1.0"));
+        workflow.name = Some(String::from(project_name));
+
+        dbg!(&workflow);
+        let manifest_json = serde_json::to_string(&workflow).unwrap();
+        dbg!(&manifest_json);
+        let manifest: Workflow = serde_yaml::from_str(&manifest_json).unwrap();
+        dbg!(&manifest);
+
+        let test_manifest_path = self.dest_path.join(project_name).join("polywrap.test.yaml");
+        let f = fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open(&test_manifest_path)
+            .unwrap();
+
+        serde_yaml::to_writer(f, &workflow);
+
+
+        dbg!(test_manifest_path);
+
+        // fs::write(test_manifest_path, manifest).expect("TODO: panic message");
+
+
+        Ok(())
     }
 }
 
@@ -134,7 +100,7 @@ fn main() -> std::io::Result<()> {
     let dirname = current_exe()?;
 
     let dest_path = Path::new(BUILD_FOLDER);
-    let source_path = Path::new("..").join(TEST_FOLDER);
+    let source_path = Path::new(TEST_FOLDER);
 
     match fs::create_dir(BUILD_FOLDER) {
         Err(_) => {
@@ -145,15 +111,11 @@ fn main() -> std::io::Result<()> {
     }
 
 
-    let generator = Generator::new(dest_path, source_path.as_path());
+    let generator = Generator::new(dest_path, source_path);
 
-    for entry in read_dir(&source_path)? {
+    for entry in fs::read_dir(&source_path)? {
           generator.generate_project(entry?.file_name().to_str().unwrap()).unwrap();
     }
-
-
-
-
 
 
     Ok(())
