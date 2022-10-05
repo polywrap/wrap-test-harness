2e6f62e2a6593bb23bf654636c073e578ae167e0
cbrzn <cesarbrazon10@gmail.com>
2022-10-06 00:39:48 +0200

    feat: add clap to name arguments, still in progress

--- src/main.rs
+++ src/main.rs
@@ -5,34 +5,52 @@ mod constants;
 mod result;
 mod engine;
 
-use std::{env, fs, io};
+use std::{fs, io};
 use std::path::Path;
-use crate::engine::{Engine, Executor};
+use clap::{Parser, Arg, command, ArgAction};
 
+use crate::engine::{Engine, Executor};
 use crate::generator::{Generator};
 use crate::result::{Results};
 
 static BUILD_FOLDER: &str = "build";
 static TEST_FOLDER: &str = "tests";
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    implementation: Option<String>,
+    feature: Option<String>,
+    verbose: Option<bool>,
+    reset: Option<bool>
+}
+
 fn main() -> io::Result<()> {
     let dest_path = Path::new(BUILD_FOLDER);
     let source_path = Path::new(TEST_FOLDER);
 
-    let mut args = env::args();
-    args.next();
-    let case_name = match args.next() {
-        Some(arg) => arg,
-        None => String::new()
-    };
-
-    match fs::create_dir(BUILD_FOLDER) {
-        Err(_) => {
-            fs::remove_dir_all(BUILD_FOLDER)?;
-            fs::create_dir(BUILD_FOLDER)?;
-        }
-        _ => {}
-    }
+    let args = Args::parse();
+    dbg!(args);
+    let case_name = String::new();
+    // let case_name = match args.feature {
+    //     Some(arg) => arg,
+    //     None => String::new()
+    // };
+    //
+    // let implementation = match args.implementation {
+    //     Some(arg) => arg,
+    //     None => String::new()
+    // };
+    //
+    // if prune.is_some() {
+    //     match fs::create_dir(BUILD_FOLDER) {
+    //         Err(_) => {
+    //             fs::remove_dir_all(BUILD_FOLDER)?;
+    //             fs::create_dir(BUILD_FOLDER)?;
+    //         }
+    //         _ => {}
+    //     }
+    // };
 
     let generator = Generator::new(dest_path, source_path);
     let mut engine = Engine::new();
