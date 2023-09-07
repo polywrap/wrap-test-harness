44afd280ab32fee2a496fab4e5fdc64d17f30908
dOrgJelli <jelli@dorg.tech>
2023-09-07 05:07:55 +0200

    chore: fix subinvoke case

--- cases/subinvoke/01-subinvoke/implementations/go/module.go
+++ cases/subinvoke/01-subinvoke/implementations/go/module.go
@@ -1,6 +1,7 @@
 package module
 
 import (
+	"github.com/polywrap/go-wrap/wrap"
 	"github.com/polywrap/wrap-test-harness/go/module/wrap/types"
 	. "github.com/polywrap/wrap-test-harness/go/module/wrap/imported/imported_subinvoke"
 )
@@ -24,4 +25,29 @@ func SubinvokeThrowError(args *types.ArgsSubinvokeThrowError) bool {
 		panic(err.Error())
 	}
 	return result
+}
+
+func SubinvokeMethodNotFound() bool {
+	argsAdd := &ImportedSubinvoke_ArgsAdd{
+		A: 1,
+		B: 1,
+	}
+	argsBuf := SerializeImportedSubinvoke_AddArgs(argsAdd)
+	_, err := wrap.WrapSubinvoke("authority/imported-subinvoke", "methodNotFound", argsBuf)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func SubinvokeArgsIncorrect() bool {
+	wrongArgs := &ImportedSubinvoke_ArgsInvokeThrowError{
+		Error: "Oops!",
+	}
+	argsBuf := SerializeImportedSubinvoke_InvokeThrowErrorArgs(wrongArgs)
+	_, err := wrap.WrapSubinvoke("authority/imported-subinvoke", "add", argsBuf)
+	if err != nil {
+		return false
+	}
+	return true
 }
